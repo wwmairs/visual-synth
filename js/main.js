9106b4833824d4efb52d9009b611cd20dd78a29c
wwmairs <wwmairs@gmail.com>
2018-04-13 21:15:21 -0400

    drawing steps in sequencer

--- js/main.js
+++ js/main.js
@@ -4,6 +4,31 @@ $(document).ready(() => {
   $btns = $(".osc-btn");
 
   var circles = {};
+  // num steps in sequencer
+  var numSteps = 8;
+
+  // draw sequencer step lines
+  const svgns = "http://www.w3.org/2000/svg";
+  var container = document.getElementById("canvas");
+  var svg = document.createElementNS(svgns, "svg");
+  let seqHeight = window.innerHeight - 60;
+  let seqWidth = window.innerWidth;
+  let stepWidth = seqWidth / numSteps;
+  container.appendChild(svg);
+  svg.setAttribute("width", seqWidth);
+  // let's just use that hardcoded osc-btn height
+  svg.setAttribute("height", seqHeight); 
+  for (var i = 1; i < numSteps; i++) {
+    let x = i * stepWidth;
+    let line = document.createElementNS(svgns, "line");
+    line.setAttribute("x1", x);
+    line.setAttribute("x2", x);
+    line.setAttribute("y1", 0);
+    line.setAttribute("y2", seqHeight);
+    line.setAttribute("stroke", "lightgrey");
+    svg.appendChild(line);
+  }
+
 
   // Create a new circle oscillator
   $btns.mousedown((e) => {
